e69da7398f647a460af3621a023a7d1dfa0b19e7
Leo Cheung <leozhang621@gmail.com>
2026-07-14 10:39:57 +0800

    feat: add Breadcrumb components for enhanced navigation

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/ui/badge';
+export * from './components/ui/breadcrumb';
 export * from './components/ui/button';
 export * from './components/ui/field';
 export * from './components/ui/input';
